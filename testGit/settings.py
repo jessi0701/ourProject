87942694f2ceef138cb8a8bed136683535619ddd
Kim Nok Hyeong <jessi0701@naver.com>
2019-05-07 06:18:20 +0000

    startapp

--- testGit/settings.py
+++ testGit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jc=wh4s4=$mscge2t=g$!*ikf%57)%lg*5v&$00e3bm5ir+vn9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
 ]
 
 MIDDLEWARE = [
